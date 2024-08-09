e09f0265c4b0c52a20a2c1f74f12d2bcc6c73ea8
pgrudra <pgrudrakshi@gmail.com>
2024-08-09 14:01:36 +0530

    debug anallytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { ThemeProvider } from "./ThemeContext";
 import Header from "./Header";
 import Footer from "./Footer";
+import Script from "next/script";
 
 export const metadata = {
   title: "Prajwal Rudrakshi - Personal Website",
@@ -15,18 +16,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-6HEYQW9H22"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-6HEYQW9H22');
-        </script>
-      </head>
       <body>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6HEYQW9H22');
+          `}
+        </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6HEYQW9H22"
+          strategy="afterInteractive"
+        />
         <ThemeProvider>
           <Header />
           <main>{children}</main>
